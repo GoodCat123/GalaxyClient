255876aa86d3084cd68c9046d5fa2646a5be6a71
GoodCat123 <73859713+GoodCat123@users.noreply.github.com>
2022-04-28 15:58:09 -0400

    Update ReMod.cs

--- ReModCE/ReMod.cs
+++ ReModCE/ReMod.cs
@@ -40,7 +40,7 @@ namespace ReModCE
 
         public static void OnApplicationStart()
         {
-            Harmony = MelonHandler.Mods.First(m => m.Info.Name == "ReModCE").HarmonyInstance;
+            Harmony = MelonHandler.Mods.First(m => m.Info.Name == "GalaxyClient").HarmonyInstance;
             Directory.CreateDirectory("UserData/ReModCE");
             ReLogger.Msg("Initializing...");
 
